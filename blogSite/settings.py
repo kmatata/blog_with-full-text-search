8b914cb64280b05f51cb945b35c5625006969b5d
kmatata <95537935+kmatata@users.noreply.github.com>
2023-03-27 14:37:17 +0300

    Update settings.py

--- blogSite/settings.py
+++ blogSite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-23p)3e1t=mx1nbb84#=sha!%e3q7-s%e7pi^3uokwkrhuaeg1e"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -112,8 +112,8 @@ USE_TZ = True
 
 # emails server config
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = '254khoi@gmail.com'
-EMAIL_HOST_PASSWORD = 'hrwqumridtsslsev'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
